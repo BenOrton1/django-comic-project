84c7e250d91a03492b6f9a58be0b56fccf73e610
Ubuntu <ubuntu@ip-172-31-27-203.us-west-2.compute.internal>
2019-09-14 09:47:18 +0000

    hire and bout pages changed for django and views added, urls updated in html and in urls.py

--- comicsite/urls.py
+++ comicsite/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from home.views import index
+from home.views import index, hire, about
 from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',index, name='index'),
+    url(r'^$', index, name='index'),
+    url(r'^hire$', hire, name='hire'),
+    url(r'^about$', about, name='about'),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     """A view that displays the index page"""
     return render(request, "index.html")
+
+def hire(request):
+    """A view that displays the hire page"""
+    return render(request, "hire.html")
+    
+def about(request):
+    """A view that displays the about page"""
+    return render(request, "about.html")
