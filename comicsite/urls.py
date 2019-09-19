4aba7f8b6d581d20972315461f6efe6d5adf07bb
Ubuntu <ubuntu@ip-172-31-27-203.us-west-2.compute.internal>
2019-09-19 10:50:55 +0000

    copied checkout app from Ecommerce minni project

--- comicsite/urls.py
+++ comicsite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from home.views import index, hire, about
 from shop import urls as urls_shop
 from mycart import urls as urls_cart
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -28,5 +29,6 @@ urlpatterns = [
     url(r'^about$', about, name='about'),
     url(r'^shop/', include(urls_shop)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
