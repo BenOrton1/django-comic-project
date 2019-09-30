917fbd17d133d33c512cfbb8d2fe556386a855e3
Ubuntu <ubuntu@ip-172-31-27-203.us-west-2.compute.internal>
2019-09-30 11:47:20 +0000

    added requiremnts and procfile

--- env.py
+++ env.py
@@ -5,3 +5,7 @@ os.environ.setdefault("STRIPE_SECRET", "sk_test_JAuAVpNTqPYvr6FTPpQDuRCm00km1niN
 
 os.environ.setdefault("EMAIL_ADDRESS", "B.R.Orton@gmail.com")
 os.environ.setdefault("EMAIL_PASSWORD", "65KFC0jsjrtw")
+
+os.environ.setdefault("SECRET_KEY", "z2m((kqs%l&fkeqh6scuzh&i24y4qq$v-dqv55_916=ir^&nak")
+
+os.environ.setdefault("DATABASE_URL", "postgres://fxotijwqurhhom:a11145bfd4f13d6b4bf0e89418830c4ded61fc58cfd8327f8c58f27efbfb5ba6@ec2-54-235-92-43.compute-1.amazonaws.com:5432/dbruvgo57u1uip")
